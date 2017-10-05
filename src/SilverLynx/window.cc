5283cb59b680f47bd0a190efa56ca72299cb8c1d
Antonio Diaz <antoniozero@outlook.com>
2017-10-06 00:51:07 +0100

    [Window] Changed Implementation of Close, StartFrame and EndFrame.

--- src/SilverLynx/window.cc
+++ src/SilverLynx/window.cc
@@ -18,18 +18,20 @@ void Init(const int32 width,
                 const char* display_name) {
 
   Core::instance().window_.init(width, height, display_name);
+  Core::instance().d3d_.init();
 }
 
 void Close() {
-  Core::instance().window_.close();
+  Core::instance().d3d_.shutdown();
 }
 
 void StartFrame() {
-  Core::instance().window_.startRenderFrame();
+  Core::instance().window_.updateMessages();
+  Core::instance().d3d_.startRenderFrame();
 }
 
 void EndFrame() {
-  Core::instance().window_.endRenderFrame();
+  Core::instance().d3d_.endRenderFrame();
 }
 
 int32 Height() {
