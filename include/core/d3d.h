e79d4b2f0bb7a04ac10cdfeb066a8eefddd94e1c
Antonio Diaz <antoniozero@outlook.com>
2017-10-06 12:38:14 +0100

    [D3D] Finished Implementation. Separated into functions.
    - Implemented shutdown function. Now removes all objects accordingly.
    - Implemented error handling on every framework setup step.
    - Implemented VSYNC dependant setup.
    - Implemented Graphics Card Adapter info gathering, setting and setup.
    - Splitted init function into separate initialization modules to improve readability.
    - Minor cosmetic fixes.

--- include/core/d3d.h
+++ include/core/d3d.h
@@ -33,7 +33,7 @@ class DirectXFramework {
 ***                               Public methods                             ***
 *******************************************************************************/
 
-  void init();
+  bool init();
   void startRenderFrame(float r = 0.0f, float g = 0.3f, float b = 0.0f, float a = 1.0f);
   void endRenderFrame();
   void shutdown();
@@ -45,6 +45,8 @@ class DirectXFramework {
   D3DXMATRIX projection_matrix_;
   D3DXMATRIX world_matrix_;
   D3DXMATRIX ortho_matrix_;
+  SLX::int32 video_card_memory_;
+  char video_card_description_[128];
 
 /*******************************************************************************
 ***                           Private                                        ***
@@ -63,8 +65,23 @@ class DirectXFramework {
   // the swap chain is the series of buffers which take turns being rendered on.
   IDXGISwapChain *swap_chain_;
   // Render target used to render our main window.
-  ID3D11RenderTargetView *backbuffer_;
-  ID3D11DepthStencilView *depth_stencil_view;
+  ID3D11RenderTargetView *render_target_view_;
+  ID3D11DepthStencilView *depth_stencil_view_;
+
+  ID3D11Texture2D* depth_stencil_buffer_;
+  ID3D11DepthStencilState* depth_stencil_state_;
+  ID3D11RasterizerState* raster_state_;
+
+  
+  bool initVideoCard();
+  bool initSwapChainDescription();
+  bool initBackBuffer();
+  bool initDepthBufferDescription();
+  bool initDepthStencilDescription();
+  bool initStencilView();
+  bool initRaster();
+
+
 
 }; /* DirectXFramework */
 
