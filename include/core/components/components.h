82daeda970abb83987bf74b242114a70c70744dc
Antonio Diaz <antoniozero@outlook.com>
2017-10-07 23:26:29 +0100

    [new][Components] created source to be used as components container.
    - Will support different component types.
    - Union of Components.
    - User will use the Components struct in order to provide base class Object with functionality.

---
+++ include/core/components/components.h
@@ -0,0 +1,32 @@
+/** Copyright Julio Picardo and Antonio Diaz. SouthBros 2017-18, all rights reserved.
+*
+*  @project SilverLynx
+*  @authors Julio Marcelo Picardo <picardope@esat-alumni.com>
+*           Antonio Diaz <antoniozero@outlook.com>
+*
+*/
+
+#ifndef __COMPONENTS_H__
+#define __COMPONENTS_H__ 1
+
+#include "silverlynx.h"
+#include "components/transform.h"
+
+namespace SLX {
+
+  enum class ComponentType {
+    Transform
+  };
+
+  union Component {
+    TransformComponent transform;
+  };
+
+  struct Components {
+    ComponentType type;
+    Component cmp;
+  };
+
+}; /* SLX */
+
+#endif
---
+++ src/core/components/components.cc
@@ -0,0 +1,18 @@
+﻿/** Copyright Julio Picardo and Antonio Diaz. SouthBros 2017-18, all rights reserved.
+*
+*  @project SilverLynx
+*  @authors Julio Marcelo Picardo <picardope@esat-alumni.com>
+*           Antonio Diaz <antoniozero@outlook.com>
+*
+*/
+
+#include "SilverLynx/globals.h"
+#include "core/components/transform.h"
+#include "core/core.h"
+#include "core/window.h"
+
+namespace SLX {
+
+  
+
+}; /* SLX */
