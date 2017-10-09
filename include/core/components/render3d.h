5697516d975ba9d58bb57b176298041a622db428
Antonio Diaz <antoniozero@outlook.com>
2017-10-09 21:04:34 +0100

    [NEW][Render3D] Component to render geometry and material of objects.
    - Implemented functions to initialize/setup material and geometry.
    - Moved function render() from geometry to this class.

---
+++ include/core/components/render3d.h
@@ -0,0 +1,103 @@
+/** Copyright Julio Picardo and Antonio Diaz. SouthBros 2017-18, all rights reserved.
+*
+*  @project SilverLynx
+*  @authors Julio Marcelo Picardo <picardope@esat-alumni.com>
+*           Antonio Diaz <antoniozero@outlook.com>
+*
+*/
+
+#ifndef __RENDER3D_H__
+#define __RENDER3D_H__ 1
+
+#include <DirectXMath.h>
+#include "core/components/component.h"
+
+namespace SLX {
+
+  class Object;
+  class CoreGeometry;
+  class CoreMaterial;
+  class TransformComponent;
+
+class Render3DComponent : Component {
+
+  public:
+
+    /*******************************************************************************
+    ***                        Constructor and destructor                        ***
+    *******************************************************************************/
+
+
+    /// Default class constructor.
+    Render3DComponent();
+
+    /// Default class destructor.
+    ~Render3DComponent();
+
+    /*******************************************************************************
+    ***                               Public methods                             ***
+    *******************************************************************************/
+
+    ///--------------------------------------------------------------------------
+    /// @fn   void init();
+    ///
+    /// @brief  Initializes the component.
+    ///--------------------------------------------------------------------------
+    void init();
+
+    ///--------------------------------------------------------------------------
+    /// @fn   void update();
+    ///
+    /// @brief  Updates the component.
+    ///--------------------------------------------------------------------------
+    void update();
+
+    ///--------------------------------------------------------------------------
+    /// @fn   void shutdown();
+    ///
+    /// @brief  Deallocates any memory and setup from this component.
+    ///--------------------------------------------------------------------------
+    void shutdown();
+
+    ///--------------------------------------------------------------------------
+    /// @fn   void setup(CoreMaterial* mat, CoreGeometry* geo);
+    ///
+    /// @brief  Setups the material and geometry for this component.
+    /// @param  mat CoreMaterial to assign to this component.
+    /// @param  geo CoreGeometry to assign to this component.
+    ///--------------------------------------------------------------------------
+    void setup(CoreMaterial* mat, CoreGeometry* geo);
+
+    ///--------------------------------------------------------------------------
+    /// @fn   void render(TransformComponent* transform);
+    ///
+    /// @brief  renders this component using the specified transform.
+    /// @param  transform TransformComponent use to obtain matrices for rendering.
+    ///--------------------------------------------------------------------------
+    void render(TransformComponent* transform);
+
+    /*******************************************************************************
+    ***                           Private                                        ***
+    *******************************************************************************/
+
+    /*******************************************************************************
+    ***                       Private Attributes                                 ***
+    *******************************************************************************/
+
+  private:
+    
+    /// Material of the component.
+    CoreMaterial* material_;
+    /// Geometry of the component.
+    CoreGeometry* geometry_;
+
+    /*******************************************************************************
+    ***                              Private methods                             ***
+    *******************************************************************************/
+
+
+  };
+
+}; /* SLX */
+
+#endif
---
+++ src/core/components/render3d.cc
@@ -0,0 +1,121 @@
+﻿/** Copyright Julio Picardo and Antonio Diaz. SouthBros 2017-18, all rights reserved.
+*
+*  @project SilverLynx
+*  @authors Julio Marcelo Picardo <picardope@esat-alumni.com>
+*           Antonio Diaz <antoniozero@outlook.com>
+*
+*/
+
+#include "core/components/render3d.h"
+#include "core/components/component.h"
+#include "core/components/transform.h"
+#include "core/geometry.h"
+#include "core/material.h"
+#include "core/object.h"
+#include "core/core.h"
+
+namespace SLX {
+
+  /*******************************************************************************
+  ***                        Constructor and destructor                        ***
+  *******************************************************************************/
+
+  Render3DComponent::Render3DComponent() : Component() {
+    type_ = ComponentType::Render3D;
+  }
+
+  Render3DComponent::~Render3DComponent() {
+    if (material_) {
+      delete material_;
+    }
+
+    if (geometry_) {
+      delete geometry_;
+    }
+  }
+
+  /*******************************************************************************
+  ***                               Public methods                             ***
+  *******************************************************************************/
+
+  void Render3DComponent::init() {
+
+    if (!material_) {
+      material_->init();
+    }
+    
+    if (!geometry_) {
+      geometry_->init();
+    }
+
+    if (material_ && geometry_) {
+      initialized_ = true;
+    }
+  }
+
+  void Render3DComponent::setup(CoreMaterial* mat, CoreGeometry* geo) {
+    if (!initialized_) {
+      init();
+    }
+
+    if (mat != nullptr) {
+      material_ = mat;
+    }
+
+    if (geometry_ != nullptr) {
+      geometry_ = geo;
+    }
+  }
+
+  void Render3DComponent::update() {
+    if (!initialized_) {
+      init();
+    }
+  }
+
+  void Render3DComponent::shutdown() {
+    if (material_) {
+      delete material_;
+    }
+
+    if (geometry_) {
+      delete geometry_;
+    }
+  }
+
+  void Render3DComponent::render(TransformComponent* transform) {
+    if (initialized_) {
+      auto* device_context = Core::instance().d3d_.deviceContext();
+      auto& cam = Core::instance().cam_;
+
+      DirectX::XMFLOAT4X4 mvp[3];
+      DirectX::XMStoreFloat4x4(mvp, transform->model_matrix());
+      DirectX::XMStoreFloat4x4(&mvp[1], DirectX::XMMatrixTranspose(Core::instance().cam_.viewMatrix()));
+      DirectX::XMStoreFloat4x4(&mvp[2], DirectX::XMMatrixTranspose(Core::instance().cam_.projectionMatrix()));
+
+      D3D11_MAPPED_SUBRESOURCE new_matrices;
+      ZeroMemory(&new_matrices, sizeof(D3D11_MAPPED_SUBRESOURCE));
+      device_context->Map(geometry_->matrix_buffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &new_matrices);
+      memcpy(new_matrices.pData, mvp, sizeof(DirectX::XMFLOAT4X4) * 3);
+      device_context->Unmap(geometry_->matrix_buffer_, 0);
+
+      uint32 stride = sizeof(VertexData);
+      uint32 offset = 0;
+
+      device_context->IASetVertexBuffers(0, 1, &geometry_->vertex_buffer_, &stride, &offset);
+      device_context->IASetIndexBuffer(geometry_->vertex_index_buffer_, DXGI_FORMAT_R32_UINT, 0);
+      device_context->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+      device_context->VSSetShader(material_->vertex_shader_, 0, 0);
+      device_context->PSSetShader(material_->pixel_shader_, 0, 0);
+      device_context->VSSetConstantBuffers(0, 1, &geometry_->matrix_buffer_);
+      device_context->IASetInputLayout(material_->input_layout_);
+      device_context->DrawIndexed(geometry_->vertex_index_.size(), 0, 0);
+    }
+  }
+
+  /*******************************************************************************
+  ***                              Private methods                             ***
+  *******************************************************************************/
+
+
+}; /* SLX */
