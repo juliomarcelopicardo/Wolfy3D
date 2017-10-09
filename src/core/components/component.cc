e98f13598a91709270a24ba24e387b9fcb531057
Antonio Diaz <antoniozero@outlook.com>
2017-10-09 21:00:50 +0100

    [NEW][Component] Base class for all new created components.
    - Contains virtual init, update and shutdown functions.
    - Attributes to check type and whether it has been initialized or not.

---
+++ src/core/components/component.cc
@@ -0,0 +1,26 @@
+﻿/** Copyright Julio Picardo and Antonio Diaz. SouthBros 2017-18, all rights reserved.
+*
+*  @project SilverLynx
+*  @authors Julio Marcelo Picardo <picardope@esat-alumni.com>
+*           Antonio Diaz <antoniozero@outlook.com>
+*
+*/
+
+#include "core/components/component.h"
+
+namespace SLX {
+
+  /*******************************************************************************
+  ***                        Constructor and destructor                        ***
+  *******************************************************************************/
+
+  Component::Component() {
+    type_ = ComponentType::None;
+    initialized_ = false;
+  }
+
+  Component::~Component() {
+
+  }
+
+}; /* SLX */
