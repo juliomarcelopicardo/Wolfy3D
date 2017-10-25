c8e4d34ff2494d6399bb3706826bd1eab25fd733
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-25 16:27:46 +0100

    [INPUT] All functions for keyboard and mouse prepared.

--- include/SilverLynx/input.h
+++ include/SilverLynx/input.h
@@ -14,8 +14,88 @@
 namespace SLX {
 namespace Input {
 
-double MousePosX();
-double MousePosY();
+/// Mouse buttons enum.
+enum MouseButton {
+  kMouseButton_Left = 0,
+  kMouseButton_Right = 1,
+  kMouseButton_Middle = 2,
+};
+/// Keyboard buttons enum.
+enum KeyboardButton {
+  kKeyboardButton_W = 87,
+  kKeyboardButton_A = 65,
+  kKeyboardButton_S = 83,
+  kKeyboardButton_D = 68,
+  kKeyboardButton_Q = 81,
+  kKeyboardButton_E = 69,
+  kKeyboardButton_Tab = 258,
+};
+
+//--------------------------------------------------------------------------
+/// @fn   const float MousePosX();
+///
+/// @brief Mouse cursor X axis position getter.
+/// @return Mouse cursor Position in the X axis.
+///--------------------------------------------------------------------------
+const float MousePosX();
+
+//--------------------------------------------------------------------------
+/// @fn   const float MousePosY();
+///
+/// @brief Mouse cursor Y axis position getter.
+/// @return Mouse cursor Position in the Y axis.
+///--------------------------------------------------------------------------
+const float MousePosY();
+
+///--------------------------------------------------------------------------
+/// @fn   const bool IsMouseButtonDown(MouseButton button);
+///
+/// @brief Mouse button down info.
+/// @param button Mouse button to check
+/// @return true if the button is down.
+///--------------------------------------------------------------------------
+const bool IsMouseButtonDown(MouseButton button);
+///--------------------------------------------------------------------------
+/// @fn   const bool IsMouseButtonUp(MouseButton button);
+///
+/// @brief Mouse button up info.
+/// @param button Mouse button to check
+/// @return true if the button is up.
+///--------------------------------------------------------------------------
+const bool IsMouseButtonUp(MouseButton button);
+///--------------------------------------------------------------------------
+/// @fn   const bool IsMouseButtonPressed(MouseButton button);
+///
+/// @brief Mouse button pressed info.
+/// @param button Mouse button to check
+/// @return true if the button is pressed.
+///--------------------------------------------------------------------------
+const bool IsMouseButtonPressed(MouseButton button);
+///--------------------------------------------------------------------------
+/// @fn   const bool IsKeyboardButtonDown(KeyboardButton button);
+///
+/// @brief Keyboard button down info.
+/// @param button Keyboard button to check
+/// @return true if the button is down.
+///--------------------------------------------------------------------------
+const bool IsKeyboardButtonDown(KeyboardButton button);
+///--------------------------------------------------------------------------
+/// @fn   const bool IsKeyboardButtonUp(KeyboardButton button);
+///
+/// @brief Keyboard button up info.
+/// @param button Keyboard button to check
+/// @return true if the button is up.
+///--------------------------------------------------------------------------
+const bool IsKeyboardButtonUp(KeyboardButton button);
+///--------------------------------------------------------------------------
+/// @fn   const bool IsKeyboardButtonPressed(KeyboardButton button);
+///
+/// @brief Keyboard button pressed info.
+/// @param button Mouse button to check
+/// @return true if the button is pressed.
+///--------------------------------------------------------------------------
+const bool IsKeyboardButtonPressed(KeyboardButton button);
+
 
 }; /* Input */
 }; /* SLX */
