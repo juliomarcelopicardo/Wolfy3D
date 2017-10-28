c83ee2b3e88601770f5c8a8e4681873ba6722016
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-28 04:16:25 +0100

    [OBJECT] Matrices updated recursively when adding a child. When adding a component, "this" will be setted as its owner.

--- src/core/object.cc
+++ src/core/object.cc
@@ -48,6 +48,7 @@ namespace SLX {
       if (!transform_) {
         transform_ = new TransformComponent;
         transform_->init();
+        transform_->set_owner(this);
         initialized_ = true;
       }
     }
@@ -74,6 +75,7 @@ namespace SLX {
         if (!render3D_) {
           render3D_ = new Render3DComponent;
           render3D_->init();
+          render3D_->set_owner(this);
         }
         break;
       }
@@ -89,7 +91,9 @@ namespace SLX {
       }
     }
     children_.push_back(obj);
+    DirectX::XMStoreFloat4x4(&obj->transform_->parent_model_matrix_, transform_->global_model_matrix());
     obj->parent_ = this;
+    obj->updateLocalModelAndChildrenMatrices();
   }
 
   void Object::updateLocalModelAndChildrenMatrices() {
@@ -97,6 +101,7 @@ namespace SLX {
     uint32 num_children = children_.size();
     for (uint32 i = 0; i < num_children; i++) {
       DirectX::XMStoreFloat4x4(&children_[i]->transform_->parent_model_matrix_, transform_->global_model_matrix());
+      children_[i]->updateLocalModelAndChildrenMatrices();
     }
   }
 
