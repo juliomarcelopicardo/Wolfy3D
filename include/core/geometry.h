708260da0ff400fa85317f46b1be5b7f8515ddc7
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-29 14:41:59 +0000

    [GEOMETRY] Added triangle geometry

--- include/core/geometry.h
+++ include/core/geometry.h
@@ -64,6 +64,16 @@ class CoreGeometry {
   ///--------------------------------------------------------------------------
   bool init(const char* height_map_filename, const float grid_size);
 
+  ///--------------------------------------------------------------------------
+  /// @fn   bool initTriangle(const float32 width, const float32 height)
+  ///
+  /// @brief  Initializes the Geometry.
+  /// @param  width Width in pixels.
+  /// @param  height Height in pixels.
+  /// @return true if successfully initialized, false otherwise.
+  ///--------------------------------------------------------------------------
+  bool initTriangle(const float32 width = 0.5f, const float32 height = 0.5f);
+
 /*******************************************************************************
 ***                               Attributes                                 ***
 *******************************************************************************/
--- src/core/geometry.cc
+++ src/core/geometry.cc
@@ -33,6 +33,93 @@ CoreGeometry::~CoreGeometry() {
 ***                               Public methods                             ***
 *******************************************************************************/
 
+bool CoreGeometry::initTriangle(const float32 width, const float32 height) {
+
+  // CUBO de ejemplo
+  int32 num_vertices = 3;
+  int32 num_indices = 3;
+
+  // Rellenamos info de vertices e indices.
+  vertex_data_.resize(num_vertices);
+  vertex_index_.resize(num_indices);
+
+  vertex_data_[0] = { { 0.0f, width * 0.5f, 0.0f },     // Position
+                      { 0.0f, 0.0f, 1.0f },             // Normal
+                      { 0.0f, 1.0f },                   // UV / TexCoord
+                      { 1.0f, 1.0f, 1.0f, 1.0f } };     // Color
+  vertex_data_[1] = { { width * 0.5f, -height * 0.5f, 0.0f },
+                      { 0.0f, 1.0f, 0.0f },
+                      { 1.0f, 0.0f },
+                      { 1.0f, 1.0f, 1.0f, 1.0f } };
+  vertex_data_[2] = { { -width * 0.5f, -height * 0.5f, 0.0f },
+                      { 1.0f, 0.0f, .0f },
+                      { 0.0f, 1.0f },
+                      { 1.0f, 1.0f, 1.0f, 1.0f } };
+  
+  vertex_index_[0] = 0;
+  vertex_index_[1] = 1;
+  vertex_index_[2] = 2;
+  
+
+  // Creamos un buffer para subir la informacion de los vertices a la grafica.
+  D3D11_BUFFER_DESC vertex_description;
+  ZeroMemory(&vertex_description, sizeof(D3D11_BUFFER_DESC));
+
+  vertex_description.Usage = D3D11_USAGE_DEFAULT;
+  vertex_description.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+  vertex_description.ByteWidth = sizeof(VertexData) * num_vertices;
+
+  D3D11_SUBRESOURCE_DATA vertex_data;
+  ZeroMemory(&vertex_data, sizeof(D3D11_SUBRESOURCE_DATA));
+  vertex_data.pSysMem = vertex_data_.data();
+
+  HRESULT result = Core::instance().d3d_.device()->CreateBuffer(&vertex_description, &vertex_data, &vertex_buffer_);
+
+  if (FAILED(result)) {
+    MessageBox(NULL, "ERROR - Vertex buffer not created", "ERROR", MB_OK);
+    return false;
+  }
+
+  // Creamos un buffer para subir la informacion de los indices a la grafica.
+  D3D11_BUFFER_DESC index_description;
+  ZeroMemory(&index_description, sizeof(D3D11_BUFFER_DESC));
+
+  index_description.Usage = D3D11_USAGE_DEFAULT;
+  index_description.BindFlags = D3D11_BIND_INDEX_BUFFER;
+  index_description.ByteWidth = sizeof(uint32) * num_indices;
+
+  D3D11_SUBRESOURCE_DATA index_data;
+  ZeroMemory(&index_data, sizeof(D3D11_SUBRESOURCE_DATA));
+  index_data.pSysMem = vertex_index_.data();
+
+  result = Core::instance().d3d_.device()->CreateBuffer(&index_description, &index_data, &vertex_index_buffer_);
+
+  if (FAILED(result)) {
+    MessageBox(NULL, "ERROR - Vertex buffer not created", "ERROR", MB_OK);
+    return false;
+  }
+
+  // Creamos un buffer para almacenar las matrices.
+  D3D11_BUFFER_DESC matrix_description;
+  ZeroMemory(&matrix_description, sizeof(D3D11_BUFFER_DESC));
+
+  matrix_description.Usage = D3D11_USAGE_DYNAMIC;
+  matrix_description.ByteWidth = sizeof(DirectX::XMFLOAT4X4) * 3;
+  matrix_description.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+  matrix_description.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+  result = Core::instance().d3d_.device()->CreateBuffer(&matrix_description, NULL, &matrix_buffer_);
+
+  if (FAILED(result)) {
+    MessageBox(NULL, "ERROR - Matrix buffer not created", "ERROR", MB_OK);
+    return false;
+  }
+
+  topology_ = D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+  return true;
+}
+
 bool CoreGeometry::init() {
 
   // CUBO de ejemplo
@@ -49,10 +136,10 @@ bool CoreGeometry::init() {
   vertex_data_[2] = { { -0.5f, -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
   vertex_data_[3] = { { 0.5f, -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
 
-  vertex_data_[4] = { { -0.5f, 0.5f, 0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
-  vertex_data_[5] = { { 0.5f, 0.5f, 0.5f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
-  vertex_data_[6] = { { -0.5f, -0.5f, 0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
-  vertex_data_[7] = { { 0.5f, -0.5f, 0.5f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
+  vertex_data_[4] = { { -0.5f, 0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
+  vertex_data_[5] = { { 0.5f, 0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
+  vertex_data_[6] = { { -0.5f, -0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 1.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
+  vertex_data_[7] = { { 0.5f, -0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
 
   vertex_data_[8] = { { -0.5f, 0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
   vertex_data_[9] = { { -0.5f, -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, { 1.0f, 1.0f, 1.0f, 1.0f } };
