8bde6d2e78fa8492bcec8f83ca513e63841269bc
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-11-12 15:30:04 +0000

    [USERMATERIAL] Material class and subclasses (diffuse, textured and normals) added to the project.

---
+++ include/SilverLynx/material.h
@@ -0,0 +1,99 @@
+/** Copyright Julio Picardo and Antonio Diaz. SouthBros 2017-18, all rights reserved.
+*
+*  @project SilverLynx
+*  @authors Julio Marcelo Picardo <picardope@esat-alumni.com>
+*           Antonio Diaz <antoniozero@outlook.com>
+*
+*/
+
+#ifndef __SILVERLYNX_MATERIAL_H__
+#define __SILVERLYNX_MATERIAL_H__ 1
+
+#include "SilverLynx/globals.h"
+#include "core/texture.h"
+
+namespace SLX {
+
+/* 
+  MATERIAL INTERFACE
+*/
+class Material {
+
+ public: 
+  Material();
+  ~Material();
+
+  virtual void setupSuperMaterial() = 0;
+
+ private:
+  Material(const Material& copy);
+  Material& operator=(const Material& copy);
+};
+
+
+/*
+  MATERIAL TEXTURE SUBCLASS
+*/
+class MaterialTextured: public Material {
+
+public:
+  MaterialTextured();
+  ~MaterialTextured();
+
+  void setupSuperMaterial() override;
+
+private:
+
+  // Se borrara cuando se cree la capa de usuario para texturas.
+  CoreTexture* texture_;
+
+
+  MaterialTextured(const MaterialTextured& copy);
+  MaterialTextured& operator=(const MaterialTextured& copy);
+};
+
+/*
+MATERIAL DIFFUSE SUBCLASS
+*/
+class MaterialDiffuse : public Material {
+
+public:
+  MaterialDiffuse();
+  ~MaterialDiffuse();
+
+  void setupSuperMaterial() override;
+
+private:
+
+  // Se borrara cuando se cree la capa de usuario para texturas.
+  CoreTexture* texture_;
+
+
+  MaterialDiffuse(const MaterialDiffuse& copy);
+  MaterialDiffuse& operator=(const MaterialDiffuse& copy);
+};
+
+/*
+MATERIAL NORMALS SUBCLASS
+*/
+class MaterialNormals : public Material {
+
+public:
+  MaterialNormals();
+  ~MaterialNormals();
+
+  void setupSuperMaterial() override;
+
+private:
+
+  // Se borrara cuando se cree la capa de usuario para texturas.
+  CoreTexture* texture_;
+
+
+  MaterialNormals(const MaterialNormals& copy);
+  MaterialNormals& operator=(const MaterialNormals& copy);
+};
+
+}; /* SLX */
+
+#endif
---
+++ src/SilverLynx/material.cc
@@ -0,0 +1,82 @@
+/** Copyright Julio Picardo and Antonio Diaz. SouthBros 2017-18, all rights reserved.
+*
+*  @project SilverLynx
+*  @authors Julio Marcelo Picardo <picardope@esat-alumni.com>
+*           Antonio Diaz <antoniozero@outlook.com>
+*
+*/
+
+#include "SilverLynx/material.h"
+#include "core/material.h"
+#include "core/core.h"
+#include <chrono>
+#include <thread>
+
+namespace SLX {
+
+
+/*******************************************************************************
+***                      MATERIAL INTERFACE BASE CLASS                       ***
+*******************************************************************************/
+
+Material::Material() {}
+
+Material::~Material() {}
+
+/*******************************************************************************
+***                      MATERIAL TEXTURED SUB CLASS                       ***
+*******************************************************************************/
+
+MaterialTextured::MaterialTextured() : Material() {
+  texture_ = nullptr;
+}
+
+MaterialTextured::~MaterialTextured() {
+  texture_ = nullptr;
+}
+
+void MaterialTextured::setupSuperMaterial() {
+  auto& super_mat = Core::instance().super_material_;
+
+  super_mat.params_.type = MaterialType::kMaterialType_OneTexture;
+  super_mat.params_.num_textures = 1;
+  super_mat.params_.texture_handle = Core::instance().error_texture_.texture_handle_;
+  if (texture_ && texture_->texture_handle_) {
+    super_mat.params_.texture_handle = texture_->texture_handle_;
+  }
+}
+
+/*******************************************************************************
+***                        MATERIAL DIFFUSE SUB CLASS                        ***
+*******************************************************************************/
+
+MaterialDiffuse::MaterialDiffuse() : Material() {}
+
+MaterialDiffuse::~MaterialDiffuse() {}
+
+void MaterialDiffuse::setupSuperMaterial() {
+  auto& super_mat = Core::instance().super_material_;
+
+  super_mat.params_.type = MaterialType::kMaterialType_Diffuse;
+  super_mat.params_.num_textures = 0;
+  super_mat.params_.texture_handle = Core::instance().error_texture_.texture_handle_;
+}
+
+/*******************************************************************************
+***                        MATERIAL NORMALS SUB CLASS                        ***
+*******************************************************************************/
+
+MaterialNormals::MaterialNormals() : Material() {}
+
+MaterialNormals::~MaterialNormals() {}
+
+void MaterialNormals::setupSuperMaterial() {
+  auto& super_mat = Core::instance().super_material_;
+
+  super_mat.params_.type = MaterialType::kMaterialType_Normals;
+  super_mat.params_.num_textures = 0;
+  super_mat.params_.texture_handle = Core::instance().error_texture_.texture_handle_;
+}
+
+
+}; /* SLX */
