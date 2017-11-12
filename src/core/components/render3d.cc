97519272859a0c6ee0e17982e9404b8fa1f2690a
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-11-12 15:30:59 +0000

    [RENDER3D] Now it uses a user material to setup the SuperMaterial of the core.

--- src/core/components/render3d.cc
+++ src/core/components/render3d.cc
@@ -13,6 +13,7 @@
 #include "core/object.h"
 #include "core/core.h"
 
+
 namespace SLX {
 
   /*******************************************************************************
@@ -32,7 +33,7 @@ namespace SLX {
   ***                               Public methods                             ***
   *******************************************************************************/
 
-  void Render3DComponent::init(CoreMaterial* mat, CoreGeometry* geo) {
+  void Render3DComponent::init(Material* mat, CoreGeometry* geo) {
     material_ = mat;
     geometry_ = geo;
     if (!geo) { geometry_ = &Core::instance().error_geometry_; }
@@ -43,19 +44,23 @@ namespace SLX {
     if (initialized_) {
       auto* device_context = Core::instance().d3d_.deviceContext();
       auto& camera = Core::instance().cam_;
+      auto& super_mat = Core::instance().super_material_;
 
-      DirectX::XMStoreFloat4x4(&material_->params_.matrices.model, transform->global_model_matrix());
-      DirectX::XMStoreFloat4x4(&material_->params_.matrices.view, DirectX::XMMatrixTranspose(camera.viewMatrix()));
-      DirectX::XMStoreFloat4x4(&material_->params_.matrices.projection, DirectX::XMMatrixTranspose(camera.projectionMatrix()));
-      material_->params_.time = (float32)Time();
-      //material_->params_.padding = 0.0f;
-      //material_->params_.padding2 = 0.3f;
+      // Saving all the matrices and common data of the super material.
+      DirectX::XMStoreFloat4x4(&super_mat.params_.matrices.model, transform->global_model_matrix());
+      DirectX::XMStoreFloat4x4(&super_mat.params_.matrices.view, DirectX::XMMatrixTranspose(camera.viewMatrix()));
+      DirectX::XMStoreFloat4x4(&super_mat.params_.matrices.projection, DirectX::XMMatrixTranspose(camera.projectionMatrix()));
+      super_mat.params_.time = (float32)Time();
+      // Setup the super material depending on the type of user's material associated.
+      material_->setupSuperMaterial();
+      //super_mat.params_.padding = 0.0f;
+      //super_mat.params_.padding2 = 0.3f;
 
       D3D11_MAPPED_SUBRESOURCE new_buffer;
       ZeroMemory(&new_buffer, sizeof(D3D11_MAPPED_SUBRESOURCE));
-      device_context->Map(material_->matrix_buffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &new_buffer);
-      memcpy(new_buffer.pData, &material_->params_, sizeof(MaterialParams));
-      device_context->Unmap(material_->matrix_buffer_, 0);
+      device_context->Map(super_mat.matrix_buffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &new_buffer);
+      memcpy(new_buffer.pData, &super_mat.params_, sizeof(MaterialParams));
+      device_context->Unmap(super_mat.matrix_buffer_, 0);
 
       uint32 stride = sizeof(VertexData);
       uint32 offset = 0;
@@ -63,11 +68,11 @@ namespace SLX {
       device_context->IASetVertexBuffers(0, 1, &geometry_->vertex_buffer_, &stride, &offset);
       device_context->IASetIndexBuffer(geometry_->vertex_index_buffer_, DXGI_FORMAT_R32_UINT, 0);
       device_context->IASetPrimitiveTopology(geometry_->topology_);
-      device_context->VSSetShader(material_->vertex_shader_, 0, 0);
-      device_context->PSSetShader(material_->pixel_shader_, 0, 0);
-      device_context->VSSetConstantBuffers(0, 1, &material_->matrix_buffer_);
-      device_context->PSSetConstantBuffers(0, 1, &material_->matrix_buffer_);
-      device_context->IASetInputLayout(material_->input_layout_);
+      device_context->VSSetShader(super_mat.vertex_shader_, 0, 0);
+      device_context->PSSetShader(super_mat.pixel_shader_, 0, 0);
+      device_context->VSSetConstantBuffers(0, 1, &super_mat.matrix_buffer_);
+      device_context->PSSetConstantBuffers(0, 1, &super_mat.matrix_buffer_);
+      device_context->IASetInputLayout(super_mat.input_layout_);
       device_context->DrawIndexed(geometry_->vertex_index_.size(), 0, 0);
     }
   }
