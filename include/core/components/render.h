1ac2040363c4d7967d1aa3a5452e24c012894dd1
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-11-18 00:33:23 +0000

    [RENDERCOMP] device setup

--- include/core/components/render.h
+++ include/core/components/render.h
@@ -15,62 +15,70 @@ namespace W3D {
 
 class RenderComponent {
 
-  public:
+public:
 
-    /*******************************************************************************
-    ***                        Constructor and destructor                        ***
-    *******************************************************************************/
+/*******************************************************************************
+***                        Constructor and destructor                        ***
+*******************************************************************************/
 
-    /// Default class constructor.
-    RenderComponent();
+  /// Default class constructor.
+  RenderComponent();
 
-    /// Default class destructor.
-    ~RenderComponent();
+  /// Default class destructor.
+  ~RenderComponent();
 
-    /*******************************************************************************
-    ***                               Public methods                             ***
-    *******************************************************************************/
+/*******************************************************************************
+***                               Public methods                             ***
+*******************************************************************************/
 
-    ///--------------------------------------------------------------------------
-    /// @fn   void init(Material* mat, CoreGeometry* geo);
-    ///
-    /// @brief  Setups the material and geometry for this component.
-    /// @param  mat Material to assign to this component.
-    /// @param  geo CoreGeometry to assign to this component.
-    ///--------------------------------------------------------------------------
-    void init(class Material* mat, class Geo* geo);
+  ///--------------------------------------------------------------------------
+  /// @fn   void init(Material* mat, CoreGeometry* geo);
+  ///
+  /// @brief  Setups the material and geometry for this component.
+  /// @param  mat Material to assign to this component.
+  /// @param  geo CoreGeometry to assign to this component.
+  ///--------------------------------------------------------------------------
+  void init(class Material* mat, class Geo* geo);
 
-    ///--------------------------------------------------------------------------
-    /// @fn   void render(TransformComponent* transform);
-    ///
-    /// @brief  renders this component using the specified transform.
-    /// @param  transform TransformComponent use to obtain matrices for rendering.
-    ///--------------------------------------------------------------------------
-    void render(class TransformComponent* transform);
+  ///--------------------------------------------------------------------------
+  /// @fn   void render(TransformComponent* transform);
+  ///
+  /// @brief  renders this component using the specified transform.
+  /// @param  transform TransformComponent use to obtain matrices for rendering.
+  ///--------------------------------------------------------------------------
+  void render(class TransformComponent* transform);
 
-    /*******************************************************************************
-    ***                           Private                                        ***
-    *******************************************************************************/
-
-    /*******************************************************************************
-    ***                       Private Attributes                                 ***
-    *******************************************************************************/
-
-  private:
-    
-    /// Whether this component have been initialized or not
-    bool initialized_;
-    /// Material of the component.
-    class Material* material_;
-    /// Geometry of the component.
-    class Geo* geometry_;
-
-    /*******************************************************************************
-    ***                              Private methods                             ***
-    *******************************************************************************/
+/*******************************************************************************
+***                           Private                                        ***
+*******************************************************************************/
 
 
-  };
+private:
+
+/*******************************************************************************
+***                              Private methods                             ***
+*******************************************************************************/
+   
+  ///--------------------------------------------------------------------------
+  /// @fn   void setupDeviceContext();
+  ///
+  /// @brief  setup the device context sending all the info to render.
+  ///--------------------------------------------------------------------------
+  void setupDeviceContext();
+
+/*******************************************************************************
+***                       Private Attributes                                 ***
+*******************************************************************************/
+
+  /// Whether this component have been initialized or not
+  bool initialized_;
+  /// Material of the component.
+  class Material* material_;
+  /// Geometry of the component.
+  class Geo* geometry_;
+
+
+}; /* RenderComponent */
 
 }; /* W3D */
 
--- src/core/components/render.cc
+++ src/core/components/render.cc
@@ -15,70 +15,75 @@
 
 namespace W3D {
 
-  /*******************************************************************************
-  ***                        Constructor and destructor                        ***
-  *******************************************************************************/
+/*******************************************************************************
+***                        Constructor and destructor                        ***
+*******************************************************************************/
 
-  RenderComponent::RenderComponent() {
-    initialized_ = false;
+RenderComponent::RenderComponent() {
+  initialized_ = false;
+}
+
+RenderComponent::~RenderComponent() {
+	material_ = nullptr;
+	geometry_ = nullptr;
+}
+
+/*******************************************************************************
+***                               Public methods                             ***
+*******************************************************************************/
+
+void RenderComponent::init(Material* mat, Geo* geo) {
+  material_ = mat;
+  geometry_ = geo;
+  if (!geo) { geometry_ = &Core::instance().error_geometry_; }
+  if (mat) { initialized_ = true; }
+}
+
+void RenderComponent::render(TransformComponent* transform) {
+  if (initialized_) {
+      
+    auto& camera = Core::instance().cam_;
+    auto& super_mat = Core::instance().super_material_;
+
+    // Saving all the matrices and common data of the super material.
+    DirectX::XMStoreFloat4x4(&super_mat.settings_.matrices.model, transform->global_model_matrix());
+    DirectX::XMStoreFloat4x4(&super_mat.settings_.matrices.view, DirectX::XMMatrixTranspose(camera.viewMatrix()));
+    DirectX::XMStoreFloat4x4(&super_mat.settings_.matrices.projection, DirectX::XMMatrixTranspose(camera.projectionMatrix()));
+    super_mat.settings_.time = (float32)Time();
+    // Setup the super material depending on the type of user's material associated.
+    material_->setupSuperMaterial();
+    // Preparing the device context to draw.
+    setupDeviceContext();
+      
   }
+}
 
-  RenderComponent::~RenderComponent() {
-	  material_ = nullptr;
-	  geometry_ = nullptr;
-  }
+/*******************************************************************************
+***                              Private methods                             ***
+*******************************************************************************/
 
-  /*******************************************************************************
-  ***                               Public methods                             ***
-  *******************************************************************************/
+void RenderComponent::setupDeviceContext() {
+  auto* device_context = Core::instance().d3d_.deviceContext();
+  auto& super_mat = Core::instance().super_material_;
 
-  void RenderComponent::init(Material* mat, Geo* geo) {
-    material_ = mat;
-    geometry_ = geo;
-    if (!geo) { geometry_ = &Core::instance().error_geometry_; }
-    if (mat) { initialized_ = true; }
-  }
+  D3D11_MAPPED_SUBRESOURCE shader_constant_buffer;
+  ZeroMemory(&shader_constant_buffer, sizeof(D3D11_MAPPED_SUBRESOURCE));
+  device_context->Map(super_mat.matrix_buffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &shader_constant_buffer);
+  memcpy(shader_constant_buffer.pData, &super_mat.settings_, sizeof(MaterialSettings));
+  device_context->Unmap(super_mat.matrix_buffer_, 0);
 
-  void RenderComponent::render(TransformComponent* transform) {
-    if (initialized_) {
-      auto* device_context = Core::instance().d3d_.deviceContext();
-      auto& camera = Core::instance().cam_;
-      auto& super_mat = Core::instance().super_material_;
+  uint32 stride = sizeof(VertexData);
+  uint32 offset = 0;
 
-      // Saving all the matrices and common data of the super material.
-      DirectX::XMStoreFloat4x4(&super_mat.params_.matrices.model, transform->global_model_matrix());
-      DirectX::XMStoreFloat4x4(&super_mat.params_.matrices.view, DirectX::XMMatrixTranspose(camera.viewMatrix()));
-      DirectX::XMStoreFloat4x4(&super_mat.params_.matrices.projection, DirectX::XMMatrixTranspose(camera.projectionMatrix()));
-      super_mat.params_.time = (float32)Time();
-      // Setup the super material depending on the type of user's material associated.
-      material_->setupSuperMaterial();
-      //super_mat.params_.padding = 0.0f;
-      //super_mat.params_.padding2 = 0.3f;
-
-      D3D11_MAPPED_SUBRESOURCE new_buffer;
-      ZeroMemory(&new_buffer, sizeof(D3D11_MAPPED_SUBRESOURCE));
-      device_context->Map(super_mat.matrix_buffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &new_buffer);
-      memcpy(new_buffer.pData, &super_mat.params_, sizeof(MaterialParams));
-      device_context->Unmap(super_mat.matrix_buffer_, 0);
-
-      uint32 stride = sizeof(VertexData);
-      uint32 offset = 0;
-
-      device_context->IASetVertexBuffers(0, 1, &geometry_->vertex_buffer_, &stride, &offset);
-      device_context->IASetIndexBuffer(geometry_->vertex_index_buffer_, DXGI_FORMAT_R32_UINT, 0);
-      device_context->IASetPrimitiveTopology(geometry_->topology_);
-      device_context->VSSetShader(super_mat.vertex_shader_, 0, 0);
-      device_context->PSSetShader(super_mat.pixel_shader_, 0, 0);
-      device_context->VSSetConstantBuffers(0, 1, &super_mat.matrix_buffer_);
-      device_context->PSSetConstantBuffers(0, 1, &super_mat.matrix_buffer_);
-      device_context->IASetInputLayout(super_mat.input_layout_);
-      device_context->DrawIndexed(geometry_->vertex_index_.size(), 0, 0);
-    }
-  }
-
-  /*******************************************************************************
-  ***                              Private methods                             ***
-  *******************************************************************************/
-
+  device_context->IASetVertexBuffers(0, 1, &geometry_->vertex_buffer_, &stride, &offset);
+  device_context->IASetIndexBuffer(geometry_->vertex_index_buffer_, DXGI_FORMAT_R32_UINT, 0);
+  device_context->IASetPrimitiveTopology(geometry_->topology_);
+  device_context->VSSetShader(super_mat.vertex_shader_, 0, 0);
+  device_context->PSSetShader(super_mat.pixel_shader_, 0, 0);
+  device_context->VSSetConstantBuffers(0, 1, &super_mat.matrix_buffer_);
+  device_context->PSSetConstantBuffers(0, 1, &super_mat.matrix_buffer_);
+  device_context->IASetInputLayout(super_mat.input_layout_);
+  device_context->DrawIndexed(geometry_->vertex_index_.size(), 0, 0);
+}
 
 }; /* W3D */
